b4f61422af0762fd793986358ba9ac468dce59d1
Baljot Singh <baljots1000@gmail.com>
2024-04-01 16:38:01 +0530

    Updating Code

--- script.py
+++ script.py
@@ -1,13 +1,13 @@
 import subprocess
 
 # Path to the Anaconda Prompt executable
-anaconda_prompt_path = r"C:\ProgramData\anaconda3\Scripts\activate.bat"  # Replace with your Anaconda installation path
+anaconda_prompt_path = r"D:\Programming\anaconda3\Scripts\activate.bat" # Replace with your Anaconda installation path
 
 # Command to activate the base environment in Anaconda
-activate_base_cmd = f'call "{anaconda_prompt_path}" "C:\\ProgramData\\anaconda3"'
+activate_base_cmd = f'call "{anaconda_prompt_path}" "D:\\Programming\\anaconda3"'
 
 # Command to execute in the Anaconda Prompt
-your_command = f'python C:\\ProgramData\\anaconda3\\detect.py --source 0'  # Replace with the command you want to execute
+your_command = f'python D:\\Programming\\Material-Detection-And-Sorting\\yolov5-master\\detect.py --source 0' # Replace with the command you want to execute
 
 # Open Anaconda Prompt and execute command
 try:
