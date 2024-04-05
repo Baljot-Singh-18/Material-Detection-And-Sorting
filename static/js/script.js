d634873a60e1d39c9a96056a13dc210cbece29cc
Baljot Singh <baljots1000@gmail.com>
2024-04-05 17:40:15 +0530

    Updating Codes

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,40 @@
 var video = document.getElementById('webcam');
-const fileInput = document.getElementById('file');
-const resultDiv = document.getElementById('result');
+const controlsBtn = document.getElementById('controlsBtn');
+const notPlaying = document.getElementById('notPlaying');
+let stream = null; // Variable to store the video stream
 
 navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        video.srcObject = stream;
-    })
-    .catch(err => {
-        console.error('Error accessing webcam:', err);
-    });
+.then(newStream => {
+    video.srcObject = newStream;
+    stream = newStream;
+    controlsBtn.textContent = 'Stop Detection'; // Update button text
+})
+.catch(err => {
+    console.error('Error accessing webcam:', err);
+});
 
+controlsBtn.addEventListener('click', () => {
+    if (stream) {
+        // Stop video streaming
+        stream.getTracks().forEach(track => track.stop());
+        video.srcObject = null; // Clear the srcObject
+        stream = null;
+        controlsBtn.textContent = 'Start Detection'; // Update button text
+        notPlaying.style.display = 'block';
+        video.style.display = 'none';
 
-
-
+    } else {
+        // Start video streaming
+        navigator.mediaDevices.getUserMedia({ video: true })
+            .then(newStream => {
+                video.srcObject = newStream;
+                stream = newStream;
+                controlsBtn.textContent = 'Stop Detection'; // Update button text
+                video.style.display = 'block';
+                notPlaying.style.display = 'none'
+            })
+            .catch(err => {
+                console.error('Error accessing webcam:', err);
+            });
+    }
+});
